083fa58de5aeeb79065d415462f484c0ca6be32b
mishba786 <mishba@ith.tech>
2023-05-03 12:15:41 +0530

    fixed leaderboard ui issue

--- src/Pages/Pots/rewardLeaderBoard.jsx
+++ src/Pages/Pots/rewardLeaderBoard.jsx
@@ -131,7 +131,7 @@ return (
                         onChange={(e)=>{getRewardLeaderBoard(e.target.value); setLeaderSearch(e.target.value);}}
                         />
                     </div>
-                    <div className="col-sm-8">
+                    <div className="col-sm-6">
                         <div className="d-flex justify-content-end">
                         <div className="borderPink angleIcon" onClick={()=>{handlePrevIndex()}}><i class="fa fa-angle-left" aria-hidden="true"></i></div>
                         <div className="borderPink">#
